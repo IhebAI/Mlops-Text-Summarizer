04edbf40d69ead4c0f5828728f2369c8af3dab21
ihebAI <iheb.aamrii@gmail.com>
2024-05-29 07:58:53 -0700

    update prediction pipeline

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -7,7 +7,6 @@ class PredictionPipeline:
     def __init__(self):
         self.config = ConfigurationManager().get_model_evaluation_config()
 
-
     
     def predict(self,text):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
